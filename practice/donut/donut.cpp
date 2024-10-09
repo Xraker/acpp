b6297e36d061dbd376d8590302bb6b0aa1f618eb
Xraker <110412737+Xraker@users.noreply.github.com>
2024-10-09 17:42:57 +0300

    better performance

--- practice/donut/donut.cpp
+++ practice/donut/donut.cpp
@@ -4,38 +4,45 @@
 
 int main()
 {
-
     std::cout << "\033[2J\033[H";
 
-    int r{20}, thickness{15}, dots{360}, x{}, y{};
-    double angle{}, pi{3.14159265358979323846};
+    int r{20}, x{}, y{};
+    double angle{};
 
-    std::vector<std::vector<char>> grid(75, std::vector<char>(250, ' '));
+    constexpr int thickness{7}, dots{360};
+    constexpr double pi{3.14159265358979323846}, angle_increment{2 * pi / dots};
+
+    int x_length{4 * (r + thickness) - 3}, y_length{2 * (r + thickness) - 1};
+
+    std::vector<std::vector<char>> grid(y_length, std::vector<char>(x_length, ' '));
 
     for (int z{}; z < thickness; z++)
     {
         for (int i{}; i < dots; i++)
         {
-            angle = 2 * pi * i / dots;
+            angle = angle_increment * i;
             x = static_cast<int>(cos(angle) * r) * 2;
             y = static_cast<int>(sin(angle) * r);
 
-            x += 2 * r + 2 * (thickness - z);
-            y += r + (thickness - z);
+            x += 2 * r + 2 * (thickness - z) - 2;
+            y += r + (thickness - z) - 1;
 
             grid[y][x] = '$';
+            // std::cout << y << " - " << x << "\n";
         }
         r++;
     }
 
-    for (const auto &ordinat : grid)
+    while (1)
     {
-        for (const auto &apsis : ordinat)
+        for (const auto &ordinat : grid)
         {
-            std::cout << apsis;
+            for (const auto &apsis : ordinat)
+            {
+                std::cout << apsis;
+            }
+            std::cout << '\n';
         }
-        std::cout << '\n';
     }
-
     return 0;
 }
