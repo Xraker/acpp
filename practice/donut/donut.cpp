4b61556d9bdc34b06210165194782e2b33039678
Xraker <110412737+Xraker@users.noreply.github.com>
2024-10-09 05:21:52 +0300

    thickness + radius control

--- practice/donut/donut.cpp
+++ practice/donut/donut.cpp
@@ -7,18 +7,25 @@ int main()
 
     std::cout << "\033[2J\033[H";
 
-    int r{30}, dots{360};
-    double x{}, y{}, angle{}, pi{3.14159265358979323846};
+    int r{20}, thickness{15}, dots{360}, x{}, y{};
+    double angle{}, pi{3.14159265358979323846};
 
     std::vector<std::vector<char>> grid(75, std::vector<char>(250, ' '));
 
-    for (int i{}; i < dots; i++)
+    for (int z{}; z < thickness; z++)
     {
-        angle = 2 * pi * i / dots;
-        x = 2 * ((int)(cos(angle) * r + r));
-        y = (int)(sin(angle) * r + r);
+        for (int i{}; i < dots; i++)
+        {
+            angle = 2 * pi * i / dots;
+            x = static_cast<int>(cos(angle) * r) * 2;
+            y = static_cast<int>(sin(angle) * r);
 
-        grid[y][x] = '$';
+            x += 2 * r + 2 * (thickness - z);
+            y += r + (thickness - z);
+
+            grid[y][x] = '$';
+        }
+        r++;
     }
 
     for (const auto &ordinat : grid)
